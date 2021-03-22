729d75c100cd69315e861bd8daffcc3410fcea49
Robert Herber <rwherber@gmail.com>
2021-03-21 17:21:42 -0700

    chore: update google auth client id

--- src/environments/environment.common.ts
+++ src/environments/environment.common.ts
@@ -1,7 +1,7 @@
 export const commonEnvironment = {
   appTitle: 'WoW Market Watcher',
   localStoragePrefix: 'wow-market-watcher',
-  googleAuthClientId: '724056943218-o5ikb5nqhh58iaq3rilv5b1lgus01ak8.apps.googleusercontent.com',
+  googleAuthClientId: '828621249181-jkn1clbsrppigmkb9alsgeglkm6t1g5m.apps.googleusercontent.com',
   retryOptions: {
     enabled: true,
     maxRetryAttempts: 3,
