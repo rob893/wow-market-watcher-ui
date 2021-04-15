60f9d39742ab3c984789b415cff59e440c2fded8
rob893 <rwherber@gmail.com>
2021-04-15 14:32:42 +0000

    Deploying to gh-pages from @ rob893/wow-market-watcher-ui@b2d88b5f4de3e2b8a78314ba30782ae9d25b52c7 🚀

---
+++ precache-manifest.dd636605e87292b86e61206a82bb3634.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d5c70cfead8726c487a",
+    "url": "css/app.9f2f2bd0.css"
+  },
+  {
+    "revision": "532e07fcb5d147413d47",
+    "url": "css/chunk-vendors.b1d2dfb4.css"
+  },
+  {
+    "revision": "0f118259ce403274f407f5e982e681c3",
+    "url": "img/google-logo.png"
+  },
+  {
+    "revision": "588c9ef86479469fea9e81350b51edbe",
+    "url": "index.html"
+  },
+  {
+    "revision": "0ab279cd2bbef3537b08",
+    "url": "js/about.840eb241.js"
+  },
+  {
+    "revision": "3d5c70cfead8726c487a",
+    "url": "js/app.d8351e32.js"
+  },
+  {
+    "revision": "532e07fcb5d147413d47",
+    "url": "js/chunk-vendors.36454ccf.js"
+  },
+  {
+    "revision": "95cdb4550fc14fa8438c797f362a0629",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
