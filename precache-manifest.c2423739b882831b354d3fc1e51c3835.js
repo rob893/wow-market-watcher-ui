3db43ca7631cc097808ffb51b1162b85adb4cb55
rob893 <rwherber@gmail.com>
2021-09-03 05:27:26 +0000

    Deploying to gh-pages from @ rob893/wow-market-watcher-ui@efc3679e416c648addaa6a09d2a7c3288d6269c4 🚀

---
+++ precache-manifest.c2423739b882831b354d3fc1e51c3835.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee657c894c0b92270122",
+    "url": "css/app.4b9f0924.css"
+  },
+  {
+    "revision": "3b5ad6a15b8b11153ba9",
+    "url": "css/chunk-vendors.e197c9b6.css"
+  },
+  {
+    "revision": "0f118259ce403274f407f5e982e681c3",
+    "url": "img/google-logo.png"
+  },
+  {
+    "revision": "d6b5506f8ba31752d7e4fdf1e26332fb",
+    "url": "index.html"
+  },
+  {
+    "revision": "ee657c894c0b92270122",
+    "url": "js/app.5673a85d.js"
+  },
+  {
+    "revision": "3b5ad6a15b8b11153ba9",
+    "url": "js/chunk-vendors.b1d61526.js"
+  },
+  {
+    "revision": "95cdb4550fc14fa8438c797f362a0629",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.6445f7a9ac7b0af04db69b5dcdbb1df0.js"
+  "precache-manifest.c2423739b882831b354d3fc1e51c3835.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "wow-market-watcher-ui"});
